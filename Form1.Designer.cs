88ce656d8998e60c76f61653f736bd3f92006d0b
minseo22222 <leejin8522@gmail.com>
2025-10-22 12:44:16 +0900

    API 불러오기 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(225, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(498, 310);
+            this.listBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "갓겜매니저";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,68 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
 
 namespace GodGameManager
 {
     public partial class Form1 : Form
     {
+        private readonly string supabaseUrl = "https://qjusboguowpyamitokjh.supabase.co";
+        private readonly string supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqdXNib2d1b3dweWFtaXRva2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwNjU5MDIsImV4cCI6MjA3NTY0MTkwMn0._AMM8U_rwX6RLAe1ACIvdC-047nnykVaQvnfcAuXZ0Q";
+
         public Form1()
         {
             InitializeComponent();
+            LoadGames();
+        }
+        private async void LoadGames()
+        {
+            await LoadGamesAsync();
+        }
+
+        private async Task LoadGamesAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", supabaseKey);
+                client.DefaultRequestHeaders.Add("apikey", supabaseKey);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = await client.GetAsync(supabaseUrl + "/rest/v1/Games?select=*");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var json = await response.Content.ReadAsStringAsync();
+                    var games = JsonConvert.DeserializeObject<List<Games>>(json);
+
+                    listBox1.Items.Clear();
+                    foreach (var game in games)
+                    {
+                        listBox1.Items.Add($"{game.Title} - {game.Developer}");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("데이터를 불러오지 못했습니다: " + response.StatusCode);
+                }
+            }
+        }
+        public class Games
+        {
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("developer")]
+            public string Developer { get; set; }
+
+            [JsonProperty("genre")]
+            public string Genre { get; set; }
         }
     }
 }
+
